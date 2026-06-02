6c8ccdf5b3d9a9945fadc98bc98366fe1dc92d40
Simon Briere <simon.briere@usherbrooke.ca>
2026-06-02 08:02:09 -0400

    Fixed issue when loading badly formatted session.oimi file in OpenTera transfers

--- libs/servers/WatchServerOpenTera.py
+++ libs/servers/WatchServerOpenTera.py
@@ -8,6 +8,7 @@ from Globals import version_string
 
 import opentera_libraries.device.DeviceAPI as DeviceAPI
 from cryptography.fernet import Fernet
+from requests import JSONDecodeError
 from threading import Lock
 
 import logging
@@ -247,13 +248,18 @@ class WatchServerOpenTera(WatchServerBase):
                 session_file = os.path.join(dir_path, 'session.oimi')
                 session_file = session_file.replace('/', os.sep)
                 if not os.path.isfile(session_file):
-                    logging.error('No session file in ' + dir_path)
+                    logging.error('No session file in ' + dir_path + ' - will try later.')
                     continue  # ... with next dataset!
 
                 with open(session_file) as f:
                     session_data = f.read()
 
-                session_data_json = json.loads(session_data)
+                try:
+                    session_data_json = json.loads(session_data)
+                except JSONDecodeError:
+                    logging.error('Unable to load session infos file in ' + dir_path + ' - ignoring dataset.')
+                    self.move_folder(dir_path, dir_path.replace('ToProcess', 'Rejected'))
+                    continue # ... with next dataset
 
                 # Check if we have all the required files for that session
                 if 'files' in session_data_json:
@@ -280,7 +286,7 @@ class WatchServerOpenTera(WatchServerBase):
                 log_file = os.path.join(dir_path, 'watch_logs.txt')
                 log_file = log_file.replace('/', os.sep)
                 if not os.path.isfile(log_file):
-                    logging.error('No watch logs file in ' + dir_path)
+                    logging.error('No watch logs file in ' + dir_path + ' - will try again later.')
                     continue  # ... with next dataset!
 
                 with open(log_file) as f:
